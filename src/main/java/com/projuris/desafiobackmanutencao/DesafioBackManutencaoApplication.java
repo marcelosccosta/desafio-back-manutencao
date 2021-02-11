ed8c21de81d88b72fab44e262bc2cd26d28a5b29
Marcelo Costa <marcelosantos20-13@hotmail.com>
2021-02-11 20:09:46 -0300

    EXPORTANDP BASE DO PROJETO

---
+++ src/main/java/com/projuris/desafiobackmanutencao/DesafioBackManutencaoApplication.java
@@ -0,0 +1,13 @@
+package com.projuris.desafiobackmanutencao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackManutencaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackManutencaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projuris/desafiobackmanutencao/DesafioBackManutencaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projuris.desafiobackmanutencao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackManutencaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
